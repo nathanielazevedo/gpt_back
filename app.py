2dc66b94b826ea7890948aebaf74bfd82ee47b86
nateazevedo <nathanielazevedo@hotmail.com>
2023-04-16 12:41:21 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import openai
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+CORS(app)
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+@app.route("/", methods = ['POST'])
+def hello_world():
+    try:       
+      response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=request.json['prompt'],
+        temperature=0.7,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+      )
+      return {"message": response}
+    except:
+      print('error')
+      return {"message": "error"}
